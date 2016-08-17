68e8d861eda4400a09f07b3714706f7798174e9e
Josh Terrill <joshterrill.dev@gmail.com>
2016-08-17 12:52:01 -0700

    added support for iframes if they are on the same domain

--- background.js
+++ background.js
@@ -1,4 +1,7 @@
 $(function() {
   // where the magic happens....
   $("*").css("border", "4px red solid");
+  if ($("iframe").length > 0) {
+    $("iframe").contents().find("*").css("border", "4px red solid");
+  }
 });
